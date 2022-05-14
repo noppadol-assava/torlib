c32ce5550f767b3b196b07567739828f602e0ccf
NoppadolAssava <little_tor@hotmail.com>
2022-05-15 01:25:11 +0900

    try new container

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -72,9 +72,9 @@ for current_version in ${versions}; do
       sphinx-build -b html docs/ docs/_build/html/${current_language}/${current_version} -D language="${current_language}"
  
       # PDF #
-      sphinx-build -b rinoh docs/ docs/_build/rinoh -D language="${current_language}"
-      mkdir -p "${docroot}/${current_language}/${current_version}"
-      cp "docs/_build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/torlib-docs_${current_language}_${current_version}.pdf"
+      # sphinx-build -b rinoh docs/ docs/_build/rinoh -D language="${current_language}"
+      # mkdir -p "${docroot}/${current_language}/${current_version}"
+      # cp "docs/_build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/torlib-docs_${current_language}_${current_version}.pdf"
  
       # EPUB #
       sphinx-build -b epub docs/ docs/_build/epub -D language="${current_language}"
